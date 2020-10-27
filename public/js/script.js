cd32f9b6c428edb260fed91b34e3ef410b37e884
Katherine Mundale <katherine.doris.mundale@gmail.com>
2020-10-27 14:21:54 -0400

    Integrated Dad joke generaton into projects section

--- public/js/script.js
+++ public/js/script.js
@@ -219,3 +219,23 @@ window.onclick = function(event) {
 
   initCarousel();
 }(document));
+
+function getJoke(){
+  var xmlhttp = new XMLHttpRequest();
+  var url = "https://icanhazdadjoke.com/";
+
+  xmlhttp.open("GET", url, true);
+  xmlhttp.setRequestHeader('User-Agent', "https://kdmundale.github.io/justCause/");
+  xmlhttp.setRequestHeader("Accept", "application/json");
+  xmlhttp.send();
+  xmlhttp.onreadystatechange = function(){
+    if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+      var result= xmlhttp.responseText;
+      var jsResult = JSON.parse(result);
+      var dadJoke = jsResult.joke;
+      document.getElementById('jokeDisplay').style.display = "inline-block";
+      document.getElementById('jokeDisplay').innerHTML = dadJoke;
+      document.getElementById('bigpush').innerHTML = 'HIT ME AGAIN';
+    };
+  };
+};
